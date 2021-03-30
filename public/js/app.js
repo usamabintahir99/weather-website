a1c77704ac8ea9b622f4efc7737f7c9646393c2f
exchangeit-pucit <exchangeit101@gmail.com>
2021-03-30 22:07:58 +0500

    modified

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,50 @@
-const weatherForm = document.querySelector('form')
-const search = document.querySelector('input')
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
+const weatherForm = document.querySelector("form")
+const search = document.querySelector("input")
+const messageOne = document.querySelector("#message-1")
+const messageTwo = document.querySelector("#message-2")
+const messageThree = document.querySelector("#message-3")
+const messageFour = document.querySelector("#message-4")
+const messageFive = document.querySelector("#message-5")
+const titleOne = document.querySelector("#title-1")
+const titleTwo = document.querySelector("#title-2")
+const titleThree = document.querySelector("#title-3")
+const titleFour = document.querySelector("#title-4")
+const titleFive = document.querySelector("#title-5")
+const degreeCelsius = [...document.getElementsByClassName("symbol")]
 
-weatherForm.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    
-    const location = search.value
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault()
 
-    messageOne.textContent = 'Loading...'
+  const location = search.value
 
-    fetch('/weather?address='+location).then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){
-            messageOne.textContent = data.error
-        }
-        else{
-            messageOne.textContent = data.location
-            messageTwo.textContent = data.forecast
-        }
+  messageOne.textContent = "Loading..."
+
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error
+        messageTwo.textContent = ""
+        messageThree.textContent = ""
+        messageFour.textContent = ""
+        messageFive.textContent = ""
+
+        titleOne.style.visibility = titleTwo.style.visibility = titleThree.style.visibility = titleFour.style.visibility = titleFive.style.visibility =
+          "hidden"
+
+        degreeCelsius.map((x) => (x.style.visibility = "hidden"))
+      } else {
+        const arr = data.forecast.split(",")
+        messageOne.textContent = data.location
+        messageTwo.textContent = arr[0]
+        messageThree.textContent = arr[1]
+        messageFour.textContent = arr[2]
+        messageFive.textContent = arr[3][0].toUpperCase() + arr[3].substring(1)
+
+        titleOne.style.visibility = titleTwo.style.visibility = titleThree.style.visibility = titleFour.style.visibility = titleFive.style.visibility =
+          "visible"
+
+        degreeCelsius.map((x) => (x.style.visibility = "visible"))
+      }
     })
-})
+  })
 })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,22 +1,32 @@
-const request = require('request')
+const request = require("request")
 
-const forecast = (longitude, latitude, callback)=>{
-    const url = 'https://api.openweathermap.org/data/2.5/onecall?lat='+latitude+'&lon='+longitude+'&exclude=minutely,hourly,alerts&units=metric&appid=101b5ba5d7ca9e29542a02ddb708d804'
+const forecast = (longitude, latitude, callback) => {
+  const url =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    latitude +
+    "&lon=" +
+    longitude +
+    "&exclude=minutely,hourly,alerts&units=metric&appid=101b5ba5d7ca9e29542a02ddb708d804"
 
-    request({url,json:true}, (error,{body})=>{
-        if(error){
-            callback('Unable to connect to weather services',undefined)
-        }
-        else if(body.cod){
-            callback('Unable to find location',undefined)
-        }
-        else{
-            console.log(body.daily[0])
-            callback(undefined, 'Its '+ body.current.temp + ' degrees currently. '+'Maximum Temperature is '+body.daily[0].temp.max+ ' degrees. '+'Minimum Temperature is '+body.daily[0].temp.min+ ' degrees. '+'Description: ' + body.daily[0].weather[0].description)
-        }
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather services", undefined)
+    } else if (body.cod) {
+      callback("Unable to find location", undefined)
+    } else {
+      //console.log(body.daily[0])
+      callback(
+        undefined,
+        +body.current.temp +
+          "," +
+          body.daily[0].temp.max +
+          "," +
+          body.daily[0].temp.min +
+          "," +
+          body.daily[0].weather[0].description
+      )
     }
-)
-
+  })
 }
 
 module.exports = forecast
